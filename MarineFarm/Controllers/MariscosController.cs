341ae9dc17bcc8b7d10c26953ee6ef8b9fbb891e
Hector Contreras <hikdul.lio@gmail.com>
2022-03-02 16:49:13 -0500

    probando vistas para mariscos

---
+++ MarineFarm/Controllers/MariscosController.cs
@@ -0,0 +1,180 @@
+﻿using AutoMapper;
+using MarineFarm.Data;
+using MarineFarm.DTO;
+using MarineFarm.Entitys;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace MarineFarm.Controllers
+{
+    /// <summary>
+    /// Controlador para manipular la data de los mariscos como elemento informativo
+    /// </summary>
+    public class MariscosController : Controller
+    {
+
+
+        #region ctor
+
+        private readonly ApplicationDbContext context;
+        private readonly IMapper mapper;
+        /// <summary>
+        /// ctor
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="mapper"></param>
+        public MariscosController(ApplicationDbContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        #endregion
+
+        #region vista inicial
+
+        /// <summary>
+        /// Vista del listado de mariscos activos
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IActionResult> Index()
+        {
+            List<GTipoDTO_out> ret = new();
+            try
+            {
+                var ents = await context
+                    .Mariscos
+                    .Where(x => x.act == true)
+                    .ToListAsync();
+
+                ret = mapper.Map<List<GTipoDTO_out>>(ents);
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+            }
+
+            return View(ret);
+        }
+
+        #endregion
+
+
+        #region Crear
+
+        /// <summary>
+        /// Crear Nuevo Elemento
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Para almacenar que los nuevos datos
+        /// </summary>
+        /// <param name="ins"></param>
+        /// <returns></returns>
+        public async Task<IActionResult> Guardar(GTipoDTO_in ins)
+        {
+            try
+            {
+                var ent = mapper.Map<Marisco>(ins);
+                context.Add(ent);
+                await context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+            }
+            return RedirectToAction("Index");
+        }
+
+        #endregion
+
+        #region Editar
+
+        /// <summary>
+        /// vista para editar un elemento en base a su id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public async Task<IActionResult> Editar(int id)
+        {
+            GTipoDTO_out dto = new();
+            try
+            {
+                var ent = await context
+                    .Mariscos
+                    .Where(x => x.id == id)
+                    .ToListAsync();
+
+                dto = mapper.Map<GTipoDTO_out>(ent);
+
+            }
+            catch (Exception ee)
+            {
+                Console.Error.WriteLine(ee.Message);
+            }
+            return View(dto);
+        }
+        /// <summary>
+        /// Para almacenar los datos de un nuevo elemento
+        /// </summary>
+        /// <param name="ins"></param>
+        /// <returns></returns>
+        public async Task<IActionResult> Save(GTipoDTO_edit ins)
+        {
+            try
+            {
+                var ent = await context.Mariscos
+                    .Where(ee => ee.id == ins.id)
+                    .FirstOrDefaultAsync();
+                 ent = mapper.Map(ins, ent);
+                await context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+            }
+            return RedirectToAction("Index");
+        }
+
+
+        #endregion
+
+
+        #region Delete
+        /// <summary>
+        /// Para eliminar un elemento
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                var ent = await context.Mariscos
+                    .Where(e => e.id == id)
+                    .FirstOrDefaultAsync();
+                if (ent != null)
+                {
+                    ent.act = false;
+                    await context.SaveChangesAsync();
+                }
+            }
+            catch (Exception ee)
+            {
+                Console.Error.WriteLine(ee.Message);
+            }
+            return RedirectToAction("Index");
+        }
+
+
+        #endregion
+
+
+    }
+}
---
+++ MarineFarm/DTO/GTipoDTO_edit.cs
@@ -0,0 +1,13 @@
+﻿namespace MarineFarm.DTO
+{
+    /// <summary>
+    /// Para editar este tipo de elemento
+    /// </summary>
+    public class GTipoDTO_edit: GTipoDTO_in
+    {
+        /// <summary>
+        /// id
+        /// </summary>
+        public int id { get; set; }
+    }
+}
